2dd92eea29fc3e687a258739ca259c0fd023fde6
younwony <wony9324@naver.com>
2023-03-17 20:25:50 +0900

    feat: Member Entity 설정

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # jpa
+
+
+## Member Table DDL
+```sql
+create table Member (
+ id bigint not null,
+ name varchar(255),
+ primary key(id)
+);
+```
--- src/main/java/dev/wony/jpa1/Jpa1Application.java
+++ src/main/java/dev/wony/jpa1/Jpa1Application.java
@@ -1,12 +1,27 @@
 package dev.wony.jpa1;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Jpa1Application {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa1");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa1"); // EntityManagerFactory는 애플리케이션 전체에서 하나만 생성해서 공유
+        EntityManager em = emf.createEntityManager(); // EntityManager는 데이터 변경을 위한 핵심 객체
+        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다.
+        tx.begin();  // 트랜잭션 시작
+
+        Member member = new Member();  // 비영속 상태
+        member.setId(1L);
+        member.setName("HelloJPA");
+
+        tx.commit();
+        em.persist(member);
+
+        em.close();
+        emf.close();
     }
 
 }
