a30ff4a57ed783bb6803274da2ff8ebc91b089e4
younwony <wony9324@naver.com>
2023-03-23 23:27:56 +0900

    feat: 양방향 연관관계

--- src/main/java/dev/wony/jpa1/domain/Member.java
+++ src/main/java/dev/wony/jpa1/domain/Member.java
@@ -1,7 +1,9 @@
 package dev.wony.jpa1.domain;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,8 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-@Setter
 @Getter
+@Setter
 @Entity
 public class Member {
 
@@ -23,11 +25,16 @@ public class Member {
     /*@Column(name = "team_id")
     private Long teamId;*/
     @ManyToOne
-    @JoinColumn(name = "team_id") // FK column ,
+    @JoinColumn(name = "team_id") // 연관관계 주인 지정 (FK가 있는 곳), 일반적으로 연관관계 주인은 외래키를 가지고 있는 곳이다. 일반적으로 team없이는 member가 존재할 수 없다.
     private Team team;
     private String username;
     private String city;
     private String street;
     private String zipcode;
 
+    // 연관관계 편의 메소드, 양방향 연관관계일 때 사용한다. 양방향 연관관계일 때는 양쪽에 값을 세팅해줘야 한다.
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
--- src/main/java/dev/wony/jpa1/domain/Team.java
+++ src/main/java/dev/wony/jpa1/domain/Team.java
@@ -1,13 +1,17 @@
 package dev.wony.jpa1.domain;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,5 +22,14 @@ public class Team {
     @Column(name = "team_id", nullable = false)
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "team") // 주인이 아닌 곳에 mappedBy를 적어준다. 주인이 아닌 곳은 FK를 가지고 있지 않다. 주인이 아닌쪽은 읽기만 가능하다.
+    private List<Member> members;
 
+    // 연관관계 편의 메소드 (양방향일 때) - 주인쪽에만 적어주면 된다. 주인쪽에만 적어주면 연관관계 편의 메소드를 사용할 수 있다. Team, Member 둘 중 하나만 적어주면 된다.
+    public void addMember(Member member) {
+        members.add(member);
+        member.setTeam(this);
+    }
+
+    // toString(), lombok, JSON 생성 라이브러리 등 을 사용할 때는 주의해야 한다. 무한 루프에 빠질 수 있다.
 }
